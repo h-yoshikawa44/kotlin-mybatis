7b260a9e57ec14c00e29982c713b7fd382adf536
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-16 17:22:48 +0900

    3. MyBatis の導入 - build.gradle.ktsに依存関係の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.arenagod.gradle.MybatisGenerator") version "1.4"
     kotlin("jvm") version "1.7.10"
 }
 
@@ -12,6 +13,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mybatis:mybatis:3.5.11")
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.1")
+    implementation("mysql:mysql-connector-java:8.0.30")
+    mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
     testImplementation(kotlin("test"))
 }
 
@@ -21,4 +26,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+mybatisGenerator {
+    verbose = true
+    configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
 }

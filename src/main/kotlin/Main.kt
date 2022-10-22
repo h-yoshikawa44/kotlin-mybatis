0bf3b63a9db71c0e1c89b324db34e35f55ac4694
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-22 17:56:41 +0900

    4. MyBatisでCRUDを作成する - Selectの実装方法

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,40 @@
+import database.*
 import org.apache.ibatis.io.Resources
 import org.apache.ibatis.session.SqlSessionFactory
 import org.apache.ibatis.session.SqlSessionFactoryBuilder
 
 fun main() {
+    selectFunc()
+}
+
+fun selectFunc() {
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val user = mapper.selectByPrimaryKey(100)
+        println(user)
+    }
+
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val userList = mapper.select {
+            where { UserDynamicSqlSupport.name isEqualTo "Jiro" }
+        }
+        println(userList)
+    }
+
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val userList = mapper.select {
+            where { UserDynamicSqlSupport.age isGreaterThanOrEqualTo 25 }
+        }
+        println(userList)
+    }
+
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.count { allRows() }
+        println(count)
+    }
 }
 
 fun createSessionFactory(): SqlSessionFactory {

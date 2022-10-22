64743f68de381c3e1d5be08dc71cbb00d9b3e5c2
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-22 17:18:49 +0900

    4. MyBatisでCRUDを作成する - 準備

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import org.apache.ibatis.io.Resources
+import org.apache.ibatis.session.SqlSessionFactory
+import org.apache.ibatis.session.SqlSessionFactoryBuilder
+
+fun main() {
+}
+
+fun createSessionFactory(): SqlSessionFactory {
+    val resource = "mybatis-config.xml"
+    val inputStream = Resources.getResourceAsStream(resource)
+    return SqlSessionFactoryBuilder().build(inputStream)
+}

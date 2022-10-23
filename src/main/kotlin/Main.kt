17ffc1a2e12d62652c3f98d7d7d30cfb4ba8fc05
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-23 16:32:03 +0900

    4. MyBatisでCRUDを作成する - Insertの実装方法

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -37,6 +37,26 @@ fun selectFunc() {
     }
 }
 
+fun insertSingle() {
+    val user = User(103, "Shiro", 18, "Hello")
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.insert(user)
+        session.commit()
+        println("${count}行のレコードを挿入しました")
+    }
+}
+
+fun insertMultiple() {
+    val userList = listOf(User(104, "Goro", 15, "Hello"), User(105, "Rokuro", 13, "Hello"))
+    createSessionFactory().openSession().use { session ->
+        val mapper = session.getMapper(UserMapper::class.java)
+        val count = mapper.insertMultiple(userList)
+        session.commit()
+        println("${count}行のレコードを挿入しました")
+    }
+}
+
 fun createSessionFactory(): SqlSessionFactory {
     val resource = "mybatis-config.xml"
     val inputStream = Resources.getResourceAsStream(resource)
